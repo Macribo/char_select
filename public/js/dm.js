ff81773c6c867f300b964484f69143cbcac309b9
macribo <roibeard.mac.unfraidh@gmail.com>
2019-12-11 13:54:07 +0100

    Add Parnell bg.

--- public/js/dm.js
+++ public/js/dm.js
@@ -18,10 +18,10 @@ $(document).ready(function () {
 let deirDM = [
     `
     "Do what is beyond your strength even should you fail sometimes."
-    <br/><h4>Charles Stewart Parnell</h4><br/>`
+    <br/><h3>Charles Stewart Parnell</h3><br/>`
     ,
     
-    `According to <a href="https://storyarchaeology.com/" style="color:green;">this</a> the Irish are "Brits" who absorbed Celtic culture;
+    `According to <a href="https://storyarchaeology.com/" style="color:orangered;">this</a> the "Irish" are "Brits" who absorbed Celtic culture;
     <br>And the "Brits" are Celts who absorbed Anglo-Saxon Culture.`,
     
     `450 million years ago:<br/>The Island of Ireland was formed when 150 vertical kilometers of "Lorentia"; <br/><img id="avalonian "src="./images/backgrounds/fainne_1.png" style="height:132px; margin-left:90px;">`,
@@ -82,9 +82,10 @@ let sayParnell = 0;
         }  
         
         if(say === sayThis){
-        $('#t').on('click', function(){
+            $('#t').addClass('special-btns-during-dm-mode');
+        
+            $('#t').on('click', function(){
                 window.location.href = "https://storyarchaeology.com/";
-
         }
         )
         
@@ -94,32 +95,34 @@ let sayParnell = 0;
 render = () => {
     console.log(say)
     if(say === sayParnell){
-        $('.dm').css('background-image','url(./images/2BG.png)');
-        
-        // alert();
-        // render();
+        $('.dm').css('background-image','url(./images/0.png)');
+        $('.dm-says').css('margin-top','-62%');
+        $('.dm-says').css('color','gray');
 
+        $('.dm-says').fadeIn(3000);
+        // alert(say);
+    }
+    if(say === sayBanba){
+        $('#b').addClass('special-btns-during-dm-mode');
+
+    // alert('say banba')
+    }
+    if(say === sayThis){
+        $('#t').addClass('special-btns-during-dm-mode');
 
     }
- if(say === sayBanba){
-    $('#b').addClass('special-btns-during-dm-mode');
-
-// alert('say banba')
-}
- if(say === sayThis){
-    $('#t').addClass('special-btns-during-dm-mode');
-
-}
- if(say === saySea){
-    $('.dm').css('background-image','url(./images/bgWaves.png)');
-}
-if (say != sayBanba && say != sayThis){
-    $('#b').removeClass('special-btns-during-dm-mode');
-    $('#t').removeClass('special-btns-during-dm-mode');
-    $('.dm').css('background-image','url(./images/2BG.png)');
-    $('.dm-says').css('color','gainsboro');
-    
-}
+    if(say === saySea){
+        $('.dm').css('background-image','url(./images/bgWaves.png)');
+    }
+    if (say != sayBanba && say  != sayParnell && say  != saySea){
+        $('#b').removeClass('special-btns-during-dm-mode');
+        $('#t').removeClass('special-btns-during-dm-mode');
+        $('.dm').css('background-image','url(./images/2BG.png)');
+        $('.dm-says').css('color','gainsboro');
+        $('.dm-says').css('margin-top','0%');
+        // $('.dm-says').css('color','gray');
+        
+    }
 
     $('.dm-says').html(deirDM[say]);
 
