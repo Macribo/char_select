c148a0a4e2304dbf777b5708f7d62b36d63b57d7
ribo <roibeard.mac.unfraidh@gmail.com>
2020-01-09 21:22:48 +0100

    blank out keyboard a bit.

--- public/js/dm.js
+++ public/js/dm.js
@@ -129,12 +129,10 @@ render = () => {
     }
     if(say === sayBanba){
         $('#b').addClass('special-btns-during-dm-mode');
+        $('#b').text('b');
         $('.dm').css('background-image','url(./images/3BG.png');
-        $('.dm').css('background-image','url(./images/3BG.png');
-        $('.dm').css('background-image','url(./images/3BG.png');
-        $('.dm').css('background-image','url(./images/3BG.png');
-        $('.dm').css('background-image','url(./images/3BG.png');
-        // $('.dm-says').css('color','black');
+        $('.dm-says').css('color','white');
+        // $('.dm-says').animate({color:'white'},500);
 
     // alert('say banba')
     }
